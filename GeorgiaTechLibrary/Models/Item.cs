55b9aa54de816668679da0eee2cf0c3f2efb845f
Eugen Bobolea <eugenbobolea@outlook.com>
2018-04-24 11:37:21 +0200

    Models started

---
+++ GeorgiaTechLibrary/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeorgiaTechLibrary.Models
+{
+    public class Item
+    {
+    }
+}
