53ca67a6cef8bd22cb038ffc3fe672c191ece7b0
Eugen Bobolea <eugenbobolea@outlook.com>
2018-04-24 14:39:33 +0200

    commierttt

--- GeorgiaTechLibrary/Models/Loan.cs
+++ GeorgiaTechLibrary/Models/Loan.cs
@@ -1,6 +1,9 @@
-﻿using GeorgiaTechLibrary.Models.Members;
+﻿using GeorgiaTechLibrary.Models.Items;
+using GeorgiaTechLibrary.Models.Members;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +13,14 @@ namespace GeorgiaTechLibrary.Models
 {
     public class Loan
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ItemID { get; set; }
         public Item Item { get; set; }
         public Member Member { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsReturned { get; set; }
 
     }
 }
--- GeorgiaTechLibrary/Models/Person.cs
+++ GeorgiaTechLibrary/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace GeorgiaTechLibrary.Models
 {
     public abstract class Person
     {
+        [Key]
         public long Ssn { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
