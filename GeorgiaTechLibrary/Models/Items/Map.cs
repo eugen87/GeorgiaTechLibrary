391e3294eb1758d71e1fae9c198552fd8c091af7
Adi Cucolaș <cucolas.adi@gmail.com>
2018-04-24 14:06:07 +0200

    moved to Items folder

--- GeorgiaTechLibrary/Models/Items/Map.cs
+++ GeorgiaTechLibrary/Models/Items/Map.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GeorgiaTechLibrary.Models
+namespace GeorgiaTechLibrary.Models.Items
 {
     public class Map : Item
     {
