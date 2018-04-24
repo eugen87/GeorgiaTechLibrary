ce9d92b5f4c7068e93670d18d877f27ab4aff0a5
Adi Cucolaș <cucolas.adi@gmail.com>
2018-04-24 12:51:04 +0200

    added Item model

--- GeorgiaTechLibrary/Models/Item.cs
+++ GeorgiaTechLibrary/Models/Item.cs
@@ -5,7 +5,27 @@ using System.Threading.Tasks;
 
 namespace GeorgiaTechLibrary.Models
 {
-    public class Item
+    public abstract class Item
     {
+        public Guid Id { get; set; }
+        public ItemInfo ItemInfo { get; set; }
+        public RentStatus RentStatus { get; set; }
+        public ItemStatus ItemStatus { get; set; }
+        public ItemCondition ItemCondition { get; set; }
+    }
+
+    public enum RentStatus
+    {
+        AVAILABLE, UNAVAILABLE
+    }
+
+    public enum ItemStatus
+    {
+        RENTABLE, NONRENTABLE, WISHLIST
+    }
+
+    public enum ItemCondition
+    {
+        OK, DAMAGED, UNUSABLE, LOST
     }
 }
