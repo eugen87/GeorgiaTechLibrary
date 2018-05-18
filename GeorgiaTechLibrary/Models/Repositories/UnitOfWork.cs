e585c7381ecd9cdefb35efba2d1a3a9d722c081c
Adi Cucolaș <cucolas.adi@gmail.com>
2018-05-18 11:57:30 +0200

    added IRepository

---
+++ GeorgiaTechLibrary/Models/Repositories/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeorgiaTechLibrary.Models.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; }
+
+        public UnitOfWork(DbContext context)
+        {
+            Context = context;
+        }
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+
+        }
+    }
+}
