5cb2b3a0fe898b4704350518cf6193f991b8d213
IoanaGanescu <ioanaganescu@visual-fix.com>
2018-05-18 14:28:22 +0200

    edited properties for employee and member

--- GeorgiaTechLibrary/Models/Employees/Employee.cs
+++ GeorgiaTechLibrary/Models/Employees/Employee.cs
@@ -7,23 +7,31 @@ namespace GeorgiaTechLibrary.Models.Employees
 {
     public abstract class Employee : Person
     {
+        private long _ssn;
+        private string _name;
+        private string _address;
+        private string _picId;
+        private string _phone;
+        private string _email;
+        private string _password;
+
         public Employee(Person person)
         {
-            this.Address = person.Address;
-            this.Email = person.Email;
-            this.Name = person.Name;
-            this.Password = person.Password;
-            this.Phone = person.Phone;
-            this.PictureId = person.PictureId;
-            this.Ssn = person.Ssn;
+            _address = person.Address;
+            _email = person.Email;
+            _name = person.Name;
+            _password = person.Password;
+            _phone = person.Phone;
+            _picId = person.PictureId;
+            _ssn = person.Ssn;
         }
         public abstract string Title { get; }
-        public override long Ssn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Address { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string PictureId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Phone { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override long Ssn { get => _ssn; set => _ssn = value; }
+        public override string Name { get => _name; set => _name = value; }
+        public override string Address { get => _address; set => _address = value; }
+        public override string PictureId { get => _picId; set => _picId = value; }
+        public override string Email { get => _email; set => _email = value; }
+        public override string Phone { get => _phone; set => _phone = value; }
+        public override string Password { get => _password; set => _password = value; }
     }
 }
--- GeorgiaTechLibrary/Models/Members/Member.cs
+++ GeorgiaTechLibrary/Models/Members/Member.cs
@@ -13,26 +13,32 @@ namespace GeorgiaTechLibrary.Models.Members
         [Required]
         public LoanRule LoanRule { get; set; }
 
-        //public int LoanRuleId { get; set; }
+        private long _ssn;
+        private string _name;
+        private string _address;
+        private string _picId;
+        private string _phone;
+        private string _email;
+        private string _password;
 
         public Member(Person person)
         {
-            this.Address = person.Address;
-            this.Email = person.Email;
-            this.Name = person.Name;
-            this.Password = person.Password;
-            this.Phone = person.Phone;
-            this.PictureId = person.PictureId;
-            this.Ssn = person.Ssn;
+            _address = person.Address;
+            _email = person.Email;
+            _name = person.Name;
+            _password = person.Password;
+            _phone = person.Phone;
+            _picId = person.PictureId;
+            _ssn = person.Ssn;
             CardExpirationDate = DateTime.Now.AddYears(4);
         }
-        public override long Ssn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Address { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string PictureId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Phone { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override long Ssn { get => _ssn; set => _ssn = value; }
+        public override string Name { get => _name; set => _name = value; }
+        public override string Address { get => _address; set => _address = value; }
+        public override string PictureId { get => _picId; set => _picId = value; }
+        public override string Email { get => _email; set => _email = value; }
+        public override string Phone { get => _phone; set => _phone = value; }
+        public override string Password { get => _password; set => _password = value; }
 
     }
 }
